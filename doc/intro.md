4e49d735ba97059e0cdfb679aa0ae913e94495df
David Zaharee <dzaharee@vlacs.org>
2014-04-17 12:19:36 -0400

    remove unused files

--- doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to hatch
-
-TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
--- test/hatch/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns hatch.core-test
-  (:require [clojure.test :refer :all]
-            [hatch.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))

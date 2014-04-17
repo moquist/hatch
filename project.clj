4d7b770966fbcdecaa74f1c1c2515c2a96c99907
David Zaharee <dzaharee@vlacs.org>
2014-04-17 12:20:05 -0400

    fix name; add description & deps

--- project.clj
+++ project.clj
@@ -1,6 +1,11 @@
-(defproject hatch "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject org.vlacs/hatch "0.1.0-SNAPSHOT"
+  :description "Hatch, how we get things in and out of the hold (Datomic)."
+  :url "https://github.com/vlacs/hatch"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [com.datomic/datomic-free "0.9.4707"]
+                 [datomic-schematode "0.1.0-RC1"]]
+  :pedantic? :warn ; :abort
+  :profiles {:dev {:source-paths ["dev"]
+                   :dependencies [[org.clojure/tools.namespace "0.2.4"]]}})

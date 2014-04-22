ee10c11a01bbd41d781dbf1b6b897b18a6c5c88a
David Zaharee <david@zaharee.com>
2014-04-22 16:13:08 -0400

    bump version to 0.1.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.vlacs/hatch "0.1.1"
+(defproject org.vlacs/hatch "0.1.2"
   :description "Hatch, how we get things in and out of the hold (Datomic)."
   :url "https://github.com/vlacs/hatch"
   :license {:name "Eclipse Public License"

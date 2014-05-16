3f4112cae778ceeb1289c11216372b91477324f7
Tom Dolan <tgdolan@gmail.com>
2014-05-16 06:26:42 -0400

    Testing : Fixing comment for existing defspec.
    Now, the property-based test should run without error.
    
    --Tom

--- test/hatch/slam_test.clj
+++ test/hatch/slam_test.clj
@@ -8,9 +8,10 @@
            [clojure.test.check.properties :as prop]
            [clojure.test.check.clojure-test :refer [ defspec]]))
 
+
+;;Testing the 'assumed truth' that the output of the slam function
+;;is in fact a valid keyword.
 (defspec slam-output-is-keyword
-  "Testing the 'assumed truth' that the output of the slam function
-   is in fact a valid keyword."
   100 ;; the number of iterations for test.check to test
   (prop/for-all [v (gen/vector gen/keyword 2)] ;;gen pairs of keywords
     (keyword? (h/slam (nth v 0) (nth v 1)))))

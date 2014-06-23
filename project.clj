fe0fc6741f4e87e676cb7aba989ba5468426a001
Jon Doane <jdoane@vlacs.org>
2014-06-23 11:15:58 -0400

    Updated hatch to use only datomic pro for testing.

--- project.clj
+++ project.clj
@@ -4,10 +4,14 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [com.datomic/datomic-free "0.9.4766.11"]
+                 ;;[com.datomic/datomic-free "0.9.4766.11"]
+                 [com.datomic/datomic-pro "0.9.4766.11"]
                  [datomic-schematode "0.1.0-RC3"]]
   :pedantic? :warn ; :abort
   :plugins [[lein-cloverage "1.0.2"]]
+  :repositories [["my.datomic.com" {:url "https://my.datomic.com/repo"
+                                    :username :env/lein_datomic_repo_username
+                                    :password :env/lein_datomic_repo_password}]]
   :profiles {:dev {:source-paths ["dev"]
                    :dependencies [[org.clojure/tools.namespace "0.2.4"]
                                   [org.clojure/test.check "0.5.7"]
